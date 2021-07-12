37734b9c7e0677d329f7929569fde5a499dba99a
multiahfoon <multiahfoon@gmail.com>
2021-07-13 11:35:54 +1200

    feat: render todos from state

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './TodoItem.styles.css'
 
-function TodoItem() {
+function TodoItem({todo}) {
+  const {title, completed} = todo
+
   return (
     <div className='inputStyle'>
-      <input type="checkbox" defaultChecked=''/>
-      <p>Todo</p>
+      <input type="checkbox" defaultChecked={completed} />
+      <p>{title}</p>
       <button>Delete</button>
     </div>
   )
