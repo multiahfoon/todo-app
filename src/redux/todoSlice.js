9ada9037b851e790ab048c08267a2e02a6c9691f
multiahfoon <multiahfoon@gmail.com>
2021-07-13 11:09:01 +1200

    feat: add mock data to state

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -2,7 +2,23 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const todoSlice = createSlice({
   name: 'todos',
-  initialState: [],
+  initialState: [
+    {
+      id: 1,
+      title: 'Todo 1',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Todo 3',
+      completed: false
+    },
+    {
+      id: 3,
+      title: 'Todo 3',
+      completed: false
+    },
+  ],
   reducers: {
     addTodo: (state, action) => {
       const newTodo = {
