e523e0d3cf5b4b3dda46d517ee06aea214a43cd9
multiahfoon <multiahfoon@gmail.com>
2021-07-12 19:44:56 +1200

    feat: add boilerplate and markup

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+      <input type="text" placeholder='Add todo' />
+      <button>Add</button>
+    </div>
+  )
+}
+
+export default Form
