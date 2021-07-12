29d262b80fc8b1ee4af9ba6ec8fbe8e733aeac83
multiahfoon <multiahfoon@gmail.com>
2021-07-12 20:16:46 +1200

    feat: add new todo to redux store

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../../redux/todoSlice'
 
 function Form() {
+  const [todo, setTodo] = useState('')
+  const dispatch = useDispatch()
+
+  const handleChange = e => {
+    setTodo(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('Submit clicked')
+    dispatch(addTodo({ title: todo }))
+    setTodo(e.target[0].value = '')
+  }
+
+
   return (
     <div>
-      <input type="text" placeholder='Add todo' />
-      <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} placeholder='Add todo' />
+        <button>Add</button>
+      </form>
     </div>
   )
 }
