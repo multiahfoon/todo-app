e23acea367b5962d915a2949b0e8739031b93886
multiahfoon <multiahfoon@gmail.com>
2021-07-13 11:35:03 +1200

    feat: add todos to component's state from store

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getTodos } from '../../redux/todoSlice'
+
 import TodoItem from '../TodoItem/TodoItem'
 
 function TodoList() {
+
+  const dispatch = useDispatch()
+  const todos = useSelector(state => state.todos)
+
+  useEffect(() => {
+    dispatch(getTodos())
+  }, [dispatch])
+
   return (
     <div>
-      <TodoItem />      
+      {
+        todos.map(todo => {
+          return <TodoItem key={todo.id} todo={todo} />
+        })
+      }
     </div>
   )
 }
