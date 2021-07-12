c504e272ca194a138510d52f87af8c79e98fc2d5
multiahfoon <multiahfoon@gmail.com>
2021-07-12 19:45:51 +1200

    feat: import todo item

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoItem from '../TodoItem/TodoItem'
+
+function TodoList() {
+  return (
+    <div>
+      <TodoItem />      
+    </div>
+  )
+}
+
+export default TodoList
