b030b6b4e8a9141558748b2abbb4eb5e26aeda8a
multiahfoon <multiahfoon@gmail.com>
2021-07-12 19:46:14 +1200

    feat: import form and todo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from 'react'
+import Form from './components/Form/Form';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
     <div>
       <h1>Todo App</h1>
+      <Form />
+      <TodoList />
     </div>
   );
 }
