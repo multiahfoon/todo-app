7d1941c72570b2bac1ef7600ebc2fe80e2c6a71d
multiahfoon <multiahfoon@gmail.com>
2021-07-13 12:51:29 +1200

    feat: add function to dispatch completed state to store

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import './TodoItem.styles.css'
 import { useDispatch } from 'react-redux'
-import { deleteTodo } from '../../redux/todoSlice'
+import { deleteTodo, toggleComplete } from '../../redux/todoSlice'
 
 function TodoItem({ todo }) {
   const { id, title, completed } = todo
   const dispatch = useDispatch()
 
   const handleDeleteClick = () => {
-    dispatch(deleteTodo({id}))
+    dispatch(deleteTodo({ id }))
+  }
+
+  const handleCheckClick = () => {
+    dispatch(toggleComplete({ id: id, completed: !completed }))
   }
 
   return (
     <div className='inputStyle'>
-      <input type="checkbox" defaultChecked={completed} />
+      <input type="checkbox" onClick={handleCheckClick} defaultChecked={completed} />
       <p>{title}</p>
       <button onClick={handleDeleteClick}>Delete</button>
     </div>
