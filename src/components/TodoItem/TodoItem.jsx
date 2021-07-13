192c101a089e729820a67c878e16b0025dcee153
multiahfoon <multiahfoon@gmail.com>
2021-07-13 12:05:58 +1200

    feat: add function to dispatch deleteTodo action

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import './TodoItem.styles.css'
+import { useDispatch } from 'react-redux'
+import { deleteTodo } from '../../redux/todoSlice'
 
-function TodoItem({todo}) {
-  const {title, completed} = todo
+function TodoItem({ todo }) {
+  const { id, title, completed } = todo
+  const dispatch = useDispatch()
+
+  const handleDeleteClick = () => {
+    dispatch(deleteTodo({id}))
+  }
 
   return (
     <div className='inputStyle'>
       <input type="checkbox" defaultChecked={completed} />
       <p>{title}</p>
-      <button>Delete</button>
+      <button onClick={handleDeleteClick}>Delete</button>
     </div>
   )
 }
