43a01f51ad3ef200db9d3603d153b865e67c2c0a
multiahfoon <multiahfoon@gmail.com>
2021-07-26 04:34:40 +1200

    feat: add patch route to update todo title

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -2,13 +2,20 @@
 const express = require('express')
 const router = express.Router()
 const todos = require('../db/todos')
-const {nanoid} = require('nanoid')
+const { nanoid } = require('nanoid')
 
 router.get('/', (req, res) => res.send(todos))
 
 router.post('/', (req, res) => {
   const newTodo = { id: nanoid(), title: req.body.title, completed: false }
   todos.push(newTodo)
+  return res.send(todos)
+})
+
+router.patch('/', (req, res) => {
+  const { id, title } = req.body
+  const index = todos.findIndex(todo => todo.id === id)
+  index !== -1 ? todos[index].title = title : null
   return res.send(todos)
 })
 
