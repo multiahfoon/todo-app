bc3d9e4beaf4d0743bbe86b741732b3baf371763
multiahfoon <multiahfoon@gmail.com>
2021-07-26 04:16:59 +1200

    feat: add post route to todos

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -2,9 +2,15 @@
 const express = require('express')
 const router = express.Router()
 const todos = require('../db/todos')
+const {nanoid} = require('nanoid')
 
+router.get('/', (req, res) => res.send(todos))
 
-router.get('/', (req, res) => res.send(todos));
+router.post('/', (req, res) => {
+  const newTodo = { id: nanoid(), title: req.body.title, completed: false }
+  todos.push(newTodo)
+  return res.send(todos)
+})
 
 // export all routes that have been defined in this file
 module.exports = router
